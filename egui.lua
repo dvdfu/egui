8f98197f770e91f84c40f18ad4344812d8ff5a59
David Fu <d23fu@uwaterloo.ca>
2016-04-21 01:24:40 -0400

    mouse events, list container

--- egui.lua
+++ egui.lua
@@ -11,6 +11,8 @@ function hexToRGB(hex)
         tonumber('0x'..hex:sub(5, 6))
 end
 
+function nullFunction() end
+
 --============================================================================== RECTANGLE
 
 local Rectangle = Class {}
@@ -34,6 +36,11 @@ function Rectangle:intersectsChild(child)
         child.pos.y < self.size.y
 end
 
+function Rectangle:contains(x, y)
+    return x >= self.pos.x and x <= self.pos.x + self.size.x and
+        y >= self.pos.y and y <= self.pos.y + self.size.y
+end
+
 function Rectangle:draw(mode)
     mode = mode or 'line'
     love.graphics.rectangle(mode, self.pos.x, self.pos.y, self.size.x, self.size.y)
@@ -43,32 +50,44 @@ end
 
 local Container = Class {
     defaultProps = {
+        -- position and size
         x = 0,
         y = 0,
         width = 0,
         height = 0,
         fillWidth = false,
         fillHeight = false,
+        -- margin and spacing
         marginTop = 0,
         marginBottom = 0,
         marginLeft = 0,
         marginRight = 0,
+        separation = 0,
+        -- background and border
         backgroundVisible = true,
         backgroundColor = '#ffffff',
         borderVisible = true,
         borderColor = '#888888',
+        -- mouse events
+        onMouseEnter = nullFunction,
+        onMouseHover = nullFunction,
+        onMouseClick = nullFunction,
+        onMouseLeave = nullFunction
     }
 }
 
 function Container:init(props, children)
+    self.parent = nil
+    self.children = {}
     self.props = {}
+    self.temp = {}
+    self.layout = {}
+    
     props = props or {}
     for prop, val in pairs(Container.defaultProps) do
         self.props[prop] = props[prop] or val
     end
 
-    self.parent = nil
-    self.children = {}
     children = children or {}
     for _, child in pairs(children) do
         self:add(child)
@@ -81,9 +100,16 @@ function Container:add(child)
     child:refresh()
 end
 
+function Container:remove(child)
+    for k, v in pairs(self.children) do
+        if v == child then
+            table.remove(self.children, k)
+        end
+    end
+end
+
 function Container:refresh()
-    self.bounds = nil
-    self.innerBounds = nil
+    self.temp = {}
 
     for prop, val in pairs(self.props) do
         if prop == 'fillWidth' and val then
@@ -98,20 +124,50 @@ function Container:refresh()
     end
 end
 
+function Container:sendMouseEvent(event)
+    event.x = event.x - self.props.x - self.props.marginLeft
+    event.y = event.y - self.props.y - self.props.marginTop
+    local sent = false
+
+    for _, child in pairs(self.children) do
+        if child:getBounds():contains(event.x, event.y) then
+            child:sendMouseEvent(event)
+            sent = true
+        end
+    end
+
+    if not sent then
+        if event.pressed then
+            self.props.onMouseClick(self, event)
+        end
+    end
+end
+
 function Container:getBounds()
-    if self.bounds then return self.bounds end
-    self.bounds = Rectangle(self.props.x, self.props.y, self.props.width, self.props.height)
-    return self.bounds
+    if self.temp.bounds then return self.temp.bounds end
+    self.temp.bounds = Rectangle(self.props.x, self.props.y, self.props.width, self.props.height)
+    return self.temp.bounds
 end
 
 function Container:getInnerBounds()
-    if self.innerBounds then return self.innerBounds end
-    self.innerBounds = Rectangle(
+    if self.temp.innerBounds then return self.temp.innerBounds end
+    self.temp.innerBounds = Rectangle(
         self.props.x + self.props.marginLeft,
         self.props.y + self.props.marginRight,
         self.props.width - self.props.marginLeft - self.props.marginRight,
         self.props.height - self.props.marginTop - self.props.marginBottom)
-    return self.innerBounds
+    return self.temp.innerBounds
+end
+
+function Container:getTruePosition()
+    if self.temp.truePosition then return self.temp.truePosition end
+    self.temp.truePosition = Vector(self.props.x, self.props.y)
+    if self.parent then
+        self.temp.truePosition = self.temp.truePosition +
+            self.parent:getTruePosition() +
+            Vector(self.parent.props.marginLeft, self.parent.props.marginTop)
+    end
+    return self.temp.truePosition
 end
 
 function Container:draw()
@@ -121,17 +177,23 @@ function Container:draw()
         love.graphics.setColor(255, 255, 255)
     end
 
-    if self.props.borderVisible then
-        love.graphics.setColor(hexToRGB(self.props.borderColor))
-        self:getBounds():draw('line')
-        love.graphics.setColor(255, 255, 255)
-    end
+    -- love.graphics.push()
+    -- love.graphics.translate(self.props.x, self.props.y)
+    -- love.graphics.setColor(0, 0, 0)
+    -- local sx, sy = self:getTruePosition():unpack()
+    -- local t = '('..sx..','..sy..')'
+    -- love.graphics.print(t)
+    -- love.graphics.setColor(255, 255, 255)
+    -- love.graphics.pop()
 
     if #self.children > 0 then
         love.graphics.push()
         love.graphics.translate(
             self.props.x + self.props.marginLeft,
             self.props.y + self.props.marginTop)
+        local sx, sy = self:getTruePosition():unpack()
+        local sw, sh = self.props.width, self.props.height
+        love.graphics.setScissor(sx, sy, sw, sh)
 
         for _, child in pairs(self.children) do
             if self:getBounds():intersectsChild(child:getBounds()) then
@@ -139,13 +201,62 @@ function Container:draw()
             end
         end
 
+        love.graphics.setScissor()
         love.graphics.pop()
+    end
+
+    if self.props.borderVisible then
+        love.graphics.setColor(hexToRGB(self.props.borderColor))
+        self:getBounds():draw('line')
+        love.graphics.setColor(255, 255, 255)
     end
 end
 
---============================================================================== CONTAINER
+--============================================================================== LIST CONTAINER
 
 local ListContainer = Class { __includes = Container }
+
+function ListContainer:init(props, children)
+    Container.init(self, props, children)
+end
+
+function ListContainer:add(child)
+    if not self.temp.tail then
+        self.temp.tail = 0
+    end
+
+    child.props.y = self.temp.tail
+    Container.add(self, child)
+    self.temp.tail = self.temp.tail + child.props.height + self.props.separation
+end
+
+function ListContainer:remove(child)
+    Container.remove(self, child)
+    self:refresh()
+end
+
+function ListContainer:removeIndex(index)
+    self:remove(self.children[index])
+end
+
+function ListContainer:refresh()
+    self.temp = {}
+
+    for prop, val in pairs(self.props) do
+        if prop == 'fillWidth' and val then
+            self.props.width = self.parent:getInnerBounds().size.x
+        elseif prop == 'fillHeight' and val then
+            self.props.height = self.parent:getInnerBounds().size.y
+        end
+    end
+
+    self.temp.tail = 0
+    for _, child in pairs(self.children) do
+        child.props.y = self.temp.tail
+        child:refresh()
+        self.temp.tail = self.temp.tail + child.props.height + self.props.separation
+    end
+end
 
 return {
     Container = Container,
